ce7dcba63b0c187b99ef13f061c21747ec647d27
DESKTOP-HQHDP16\TBL <vtvhdevelopment@gmail.com>
2022-05-05 14:07:32 +0700

    80% Done on Register

--- client/src/views/register.view.tsx
+++ client/src/views/register.view.tsx
@@ -1,7 +1,5 @@
-import { X509Certificate } from 'crypto';
 import React, { useState, useEffect } from 'react'
-import { ReactDOM } from 'react';
-import { Form, Button } from 'react-bootstrap'
+import { Form, Button, Tooltip, OverlayTrigger } from 'react-bootstrap'
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import { IDistrict, IProvice, IWard } from '../models/address.model';
@@ -11,7 +9,7 @@ import '../styles/register.style.scss'
 import Validator from '../utils/validator.util';
 
 const RegisterView = () => {
-  const [startDate, setStartDate] = useState<Date|null>(null);
+  const [startDate, setStartDate] = useState<Date | null>(null);
   const [provinces, setProvinces] = useState<Array<IProvice>>([]);
   const [districts, setDistricts] = useState<Array<IDistrict>>([]);
   const [wards, setWards] = useState<Array<IWard>>([]);
@@ -19,7 +17,7 @@ const RegisterView = () => {
   const [districtCode, setDistrictCode] = useState(0);
   const [wardCode, setWardCode] = useState(0);
   const [signup, setSignup] = useState<ISignUp>(initialSignUp)
-  console.log(document.getElementById('datePicker')?.nodeValue);
+
   useEffect(() => {
     async function fetchProvinces() {
       var data = await AddressService.getProvinces();
@@ -47,6 +45,13 @@ const RegisterView = () => {
     fetchDistrictsByProvinces(provinceCode);
   }, [provinceCode, districtCode])
 
+
+  const renderTooltip = (msg: string) => (
+    <Tooltip id="button-tooltip">
+      {msg}
+    </Tooltip>
+  );
+
   async function onSelectionChange(event: React.ChangeEvent<HTMLSelectElement>) {
     var selectionID = event.currentTarget.getAttribute('id')?.toString();
     switch (selectionID) {
@@ -70,10 +75,10 @@ const RegisterView = () => {
   async function onClick(event: React.MouseEvent) {
     var btnID = event.currentTarget.getAttribute('id')?.toString();
     if (btnID === 'registerBtn') {
-      if(Validator.isValidAddress(provinceCode)
-      && Validator.isValidAddress(districtCode)
-      && Validator.isValidAddress(wardCode)
-      ){
+      if (Validator.isValidAddress(provinceCode)
+        && Validator.isValidAddress(districtCode)
+        && Validator.isValidAddress(wardCode)
+      ) {
 
       }
     }
@@ -177,53 +182,106 @@ const RegisterView = () => {
         <small>Create your account. It's free and only take a minute.</small>
         <Form.Group className="mb-3" controlId="register.username">
           <Form.Label>Username</Form.Label>
-          <Form.Control type="text" placeholder="Username" onBlur={onInputFocusOut} onChange={onInputChange} />
+          <OverlayTrigger
+            placement="right"
+            delay={{ show: 250, hide: 400 }}
+            overlay={renderTooltip("Username is required ! Username must contain dot character ! ")}
+          >
+            <Form.Control type="text" placeholder="Username" onBlur={onInputFocusOut} onChange={onInputChange} />
+          </OverlayTrigger>
         </Form.Group>
         <Form.Group className="mb-3" controlId="register.password">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" onBlur={onInputFocusOut} onChange={onInputChange} />
+          <OverlayTrigger
+            placement="right"
+            delay={{ show: 250, hide: 400 }}
+            overlay={renderTooltip("Password is required ! Password must contain at least Uppercase, special character and number !")}
+          >
+            <Form.Control type="password" placeholder="Password" onBlur={onInputFocusOut} onChange={onInputChange} />
+          </OverlayTrigger>
         </Form.Group>
         <Form.Group className="mb-3" controlId="register.fullname">
           <Form.Label>Fullname</Form.Label>
-          <Form.Control type="text" placeholder="Fullname" onBlur={onInputFocusOut} onChange={onInputChange} />
+          <OverlayTrigger
+            placement="right"
+            delay={{ show: 250, hide: 400 }}
+            overlay={renderTooltip("Fullname is required ! Fullname must not contain special characters and number ! ")}
+          >
+            <Form.Control type="text" placeholder="Fullname" onBlur={onInputFocusOut} onChange={onInputChange} />
+          </OverlayTrigger>
         </Form.Group>
         <Form.Group className="mb-3" controlId="register.email">
           <Form.Label>Email</Form.Label>
-          <Form.Control type="email" placeholder="Email" onBlur={onInputFocusOut} onChange={onInputChange} />
+          <OverlayTrigger
+            placement="right"
+            delay={{ show: 250, hide: 400 }}
+            overlay={renderTooltip("Email is required ! Email must follow [abcxyz@gmail.com] ")}
+          >
+            <Form.Control type="email" placeholder="Email" onBlur={onInputFocusOut} onChange={onInputChange} />
+          </OverlayTrigger>
         </Form.Group>
         <Form.Group className="mb-3" controlId="register.phone">
           <Form.Label>Phone Number</Form.Label>
-          <Form.Control type="tel" placeholder="Phone Number" onBlur={onInputFocusOut} onChange={onInputChange} />
+          <OverlayTrigger
+            placement="right"
+            delay={{ show: 250, hide: 400 }}
+            overlay={renderTooltip("Phone Number must contain only number !")}
+          >
+            <Form.Control type="tel" placeholder="Phone Number" onBlur={onInputFocusOut} onChange={onInputChange} />
+          </OverlayTrigger>
         </Form.Group>
-        <Form.Group className="mb-3">
-          <Form.Label>Birthday</Form.Label>
-          {/* <DatePicker selected={startDate} onChange={(date: Date) => setStartDate(date)} /> */}
-          <DatePicker id='datePicker' onChange={(date: Date) => setStartDate(date)} selected={startDate} placeholderText="Your Birthday"/>
-        </Form.Group>
+        <OverlayTrigger
+          placement="top"
+          delay={{ show: 250, hide: 400 }}
+          overlay={renderTooltip("You must select your birthday!")}
+        >
+          <Form.Group className="mb-3">
+            <Form.Label>Birthday</Form.Label>
+            <DatePicker id='datePicker' onChange={(date: Date) => setStartDate(date)} selected={startDate} placeholderText="Your Birthday" />
+          </Form.Group>
+        </OverlayTrigger>
         <Form.Group className="mb-3">
           <Form.Label>Address</Form.Label>
-          <Form.Select aria-label="Default select example" id="province" onChange={onSelectionChange} onBlur={onSelectFocusOut}>
-            <option value={0}>Province</option>
-            {
-              provinces.map((province) => (<option key={province.code} value={province.code}>{province.name}</option>))
-            }
-          </Form.Select>
-          <Form.Select aria-label="Default select example" id="district" onChange={onSelectionChange} onBlur={onSelectFocusOut}>
-            <option value={0}>District</option>
-            {
-              districts.map(district => (<option key={district.code} value={district.code}>{district.name}</option>))
-            }
-          </Form.Select>
+          <OverlayTrigger
+            placement="top"
+            delay={{ show: 250, hide: 400 }}
+            overlay={renderTooltip("You must select provice!")}
+          >
+            <Form.Select aria-label="Default select example" id="province" onChange={onSelectionChange} onBlur={onSelectFocusOut}>
+              <option value={0}>Province</option>
+              {
+                provinces.map((province) => (<option key={province.code} value={province.code}>{province.name}</option>))
+              }
+            </Form.Select>
+          </OverlayTrigger>
+          <OverlayTrigger
+            placement="top"
+            delay={{ show: 250, hide: 400 }}
+            overlay={renderTooltip("You must select district!")}
+          >
+            <Form.Select aria-label="Default select example" id="district" onChange={onSelectionChange} onBlur={onSelectFocusOut}>
+              <option value={0}>District</option>
+              {
+                districts.map(district => (<option key={district.code} value={district.code}>{district.name}</option>))
+              }
+            </Form.Select>
+          </OverlayTrigger>
+          <OverlayTrigger
+            placement="top"
+            delay={{ show: 250, hide: 400 }}
+            overlay={renderTooltip("You must select ward!")}
+          >
           <Form.Select aria-label="Default select example" id="ward" onChange={onSelectionChange} onBlur={onSelectFocusOut}>
             <option value={0}>Ward</option>
             {
               wards.map(ward => (<option key={ward.code} value={ward.code}>{ward.name}</option>))
             }
           </Form.Select>
+          </OverlayTrigger>
         </Form.Group>
         <Button id='registerBtn' variant="primary" type="submit" onClick={onClick}>Register</Button>
       </Form>
-    </div>
+    </div >
   )
 }
 
